62bfc3970ba31b42223418bd7ba3859e6bb21755
Girish Vatwani <55022371+GirishVatwani@users.noreply.github.com>
2025-07-22 20:38:19 +0530

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -4,13 +4,6 @@
 #include <string.h>
 #include <easyinputs.c>
 
-// Make sure program was run with just one command-line argument
-// Make sure every character in argv[1] is a digit
-// Convert argv[1] from a `string` to an `int`
-// Prompt user for plaintext
-// For each character in the plaintext:
-// Rotate the character if it's a letter
-
 bool only_digit(string arr[]);
 void encipher(string text, int k);
 int main(int argc, string argv[])
